155881126835cea3d6653d0e581e0050af137d26
Charles Buczel <charles.buczel@gmail.com>
2017-04-24 01:00:23 +0100

    Stub handlers

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null
+    }
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.onClick()} >
+        {this.props.value}
       </button>
     );
   }
